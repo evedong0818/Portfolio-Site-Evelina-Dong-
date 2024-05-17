d7db6e6a633e38561521d57e7bbe5788b520d608
evedong0818 <168595809+evedong0818@users.noreply.github.com>
2024-05-17 19:23:21 -0400

    exercise 3.2

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,26 @@ function menuToggle() {
       x.className = 'navtoggle';
     }
   }
+
+  // Smooth scrolling function
+function smoothScrollTo(target) {
+  var headerOffset = 43;
+
+  $("html, body").animate(
+    {
+      scrollTop: target.offset().top - headerOffset,
+    },
+    1000,
+    function () {
+      // Callback after animation
+      var $target = $(target);
+      $target.focus();
+      if ($target.is(":focus")) {
+        return false;
+      } else {
+        $target.attr("tabindex", "-1");
+        $target.focus();
+      }
+    }
+  );
+}
