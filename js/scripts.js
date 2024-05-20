9c789b37fbd00f9d980c57de2803513c994eff6d
evedong0818 <168595809+evedong0818@users.noreply.github.com>
2024-05-20 17:55:45 -0400

    fix hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 console.log("Hello! Welcome to my Portfolio Site")
+
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
@@ -8,25 +9,3 @@ function menuToggle() {
     }
   }
 
-  // Smooth scrolling function
-function smoothScrollTo(target) {
-  var headerOffset = 43;
-
-  $("html, body").animate(
-    {
-      scrollTop: target.offset().top - headerOffset,
-    },
-    1000,
-    function () {
-      // Callback after animation
-      var $target = $(target);
-      $target.focus();
-      if ($target.is(":focus")) {
-        return false;
-      } else {
-        $target.attr("tabindex", "-1");
-        $target.focus();
-      }
-    }
-  );
-}
