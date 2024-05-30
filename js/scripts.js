52d076690113bc1c072c7a8c7dc0dcbefb501d1a
evedong0818 <168595809+evedong0818@users.noreply.github.com>
2024-05-30 16:11:09 -0400

    fixed issues

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/*Menu toggle*/
+function menuToggle(x) {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
+  
+  $('#click_advance').click(function() {
+      $('#display_advance').toggle('1000');
+      $("i", this).toggleClass("fas fa-bars fas fa-x");
+  });
+  
