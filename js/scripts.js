3dd04f08971a7644b3fb1b8d13994fdb379fceb7
evedong0818 <168595809+evedong0818@users.noreply.github.com>
2024-06-02 14:59:59 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 /*Menu toggle*/
 function menuToggle(x) {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
   }
-  
-  $('#click_advance').click(function() {
-      $('#display_advance').toggle('1000');
-      $("i", this).toggleClass("fas fa-bars fas fa-x");
-  });
-  
+}
+
+$('#click_advance').click(function() {
+    $('#display_advance').toggle('1000');
+    $("i", this).toggleClass("fas fa-bars fas fa-x");
+});
